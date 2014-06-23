cfb916e1107a67fca04d58769a8b30fc1f8982cb
Brian King <brian@liip.ch>
2014-06-23 21:59:58 +0200

    SUP-5040 allow direct manipulation of course deletion settings, for testing

---
+++ coursesettings_force.php
@@ -0,0 +1,112 @@
+<?php
+require_once(__DIR__ . '/../../config.php');
+require_once("$CFG->libdir/formslib.php");
+require_once(__DIR__ . '/coursesettings_force_form.php');
+require_once(__DIR__ . '/locallib.php');
+
+$id = required_param('id', PARAM_INT);
+
+$PAGE->set_url('/local/coursedeletion/coursesettings.php', array('id'=>$id));
+
+if (! $course = $DB->get_record("course", array('id' => $id))) {
+    print_error('invalidcourseid');
+}
+require_course_login($course);
+
+// This script is only meant to be used for testing / debugging purposes.
+// If someone should be able to access it, define in config.php a value like:
+//
+//    $CFG->local_coursedeletion_force_users = 12;
+// or
+//    $CFG->local_coursedeletion_force_users = '12,99,102';
+//
+// where the numbers are user ids of the users who shall be permitted access.
+
+$can_access = false;
+if (isset($CFG->local_coursedeletion_force_users)) {
+    $userids = explode(',', clean_param($CFG->local_coursedeletion_force_users, PARAM_SEQUENCE));
+    $can_access = in_array($USER->id, $userids);
+}
+if (!$can_access) {
+    send_file_not_found();
+    exit;
+}
+
+$coursecontext = context_course::instance($course->id);
+if (!has_capability('local/coursedeletion:course_autodelete_settings', $coursecontext)) {
+    print_error(
+        'nopermissions',
+        'error',
+        new moodle_url('/course/view.php', array('id' => $course->id))        
+    );
+}
+
+if (!$coursedeletion = $DB->get_record('local_coursedeletion', array('courseid' => $id))) {
+    print_error(
+        'invalidrecordunknown',
+        'error',
+        new moodle_url('/course/view.php', array('id' => $course->id))
+    );
+}
+
+$strcoursedeletion = get_string('coursedeletionsettings', 'local_coursedeletion');
+$PAGE->set_pagelayout('incourse');
+$PAGE->set_title($strcoursedeletion);
+$PAGE->set_heading($course->fullname);
+
+$formparams = array(
+    'coursedeletion' => $coursedeletion,
+);
+$mform = new coursedeletion_coursesettings_force_form(null, $formparams);
+
+if ($mform->is_cancelled()) {
+    redirect(new moodle_url('/course/view.php', array('id' => $course->id)));
+}
+
+/* @var core_renderer $OUTPUT */
+echo $OUTPUT->header();
+echo $OUTPUT->heading(get_string('coursedeletionsettingsheader', 'local_coursedeletion'));
+
+echo $OUTPUT->container('Be careful!  Here you can directly manipulate the values; no validity checking will be done.', 'alert');
+
+$flash = array();
+
+if ($form = $mform->get_data()){
+    $coursedeletion->enddate = $form->enddate;
+    $coursedeletion->status = $form->status;
+    $DB->update_record('local_coursedeletion', $coursedeletion);
+
+    switch ($coursedeletion->status) {
+        case CourseDeletion::STATUS_NOT_SCHEDULED:
+            if (isset($info['changes']['status'])) {
+                $flash[] = get_string('deletion_not_scheduled', 'local_coursedeletion');
+            }
+            break;
+        case CourseDeletion::STATUS_SCHEDULED:
+        case CourseDeletion::STATUS_SCHEDULED_NOTIFIED:
+            $a = new stdClass;
+            if ($coursedeletion->status == CourseDeletion::STATUS_SCHEDULED) {
+                $a->maildate = CourseDeletion::first_notification_date($coursedeletion->enddate)->format('d.m.Y');
+            } else {
+                $a->maildate = get_string('already_sent', 'local_coursedeletion');
+            }
+            $a->stagedate = CourseDeletion::date_course_will_be_staged_for_deletion($coursedeletion->enddate)->format('d.m.Y');
+            $a->deletiondate = CourseDeletion::date_course_will_be_deleted($coursedeletion->enddate)->format('d.m.Y');
+            $flash[] = get_string('scheduled_upcoming_events', 'local_coursedeletion', $a);
+            break;
+    }
+}
+
+
+foreach ($flash as $message) {
+    echo $OUTPUT->container($message, 'alert alert-info');
+}
+
+$mform->set_data(array(
+    'id' => $course->id,
+    'status' => $coursedeletion->status,
+    'enddate' => $coursedeletion->enddate,
+));
+$mform->display();
+
+echo $OUTPUT->footer();
---
+++ coursesettings_force_form.php
@@ -0,0 +1,30 @@
+<?php
+class coursedeletion_coursesettings_force_form extends moodleform{
+
+    function definition() {
+        $mform =& $this->_form;
+        $coursedeletion = $this->_customdata['coursedeletion'];
+
+        $mform->addElement('hidden', 'id');
+
+        $strenddate = get_string('enddate', 'local_coursedeletion');
+        $startyear = min(date('Y') - 3, date('Y', $coursedeletion->enddate));
+        $mform->addElement('date_selector', 'enddate',
+            $strenddate,
+            array('startyear' => $startyear)
+        );
+        $mform->addRule('enddate', null, 'required');
+        $mform->addHelpButton('enddate', 'enddate', 'local_coursedeletion');
+
+        $statii = array(
+            CourseDeletion::STATUS_NOT_SCHEDULED => 'not scheduled for deletion',
+            CourseDeletion::STATUS_SCHEDULED => 'scheduled for deletion',
+            CourseDeletion::STATUS_SCHEDULED_NOTIFIED => 'scheduled, first notification sent',
+            CourseDeletion::STATUS_STAGED_FOR_DELETION => 'scheduled, already moved to trash',
+        );
+        $mform->addElement('select', 'status', 'Status', $statii);
+        $mform->addRule('status', null, 'required');
+
+        $this->add_action_buttons();
+    }
+}
