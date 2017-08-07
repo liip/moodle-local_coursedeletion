d76a4c258a7eeaab4e821792f94f76179268f56e
François Bruneau <francois.bruneau@liip.ch>
2017-08-07 17:20:35 +0200

    Fixed missing link in course settings due to Navigation API changes

--- lib.php
+++ lib.php
@@ -6,7 +6,7 @@
  * @param $settingsnav
  * @param $context
  */
-function local_coursedeletion_extends_settings_navigation($settingsnav, $context) {
+function local_coursedeletion_extend_navigation_course  (navigation_node $parentnode, stdClass $course, context_course $context) {
     global $PAGE;
 
     // Only add navigation item on non-site course pages.
@@ -18,20 +18,20 @@ function local_coursedeletion_extends_settings_navigation($settingsnav, $context
         return;
     }
 
-    if ($settingnode = $settingsnav->find('courseadmin', navigation_node::TYPE_COURSE)) {
-        $label = get_string('coursedeletionsettings', 'local_coursedeletion');
-        $url = new moodle_url('/local/coursedeletion/coursesettings.php', array('id' => $PAGE->course->id));
-        $newnode = navigation_node::create(
-            $label,
-            $url,
-            navigation_node::NODETYPE_LEAF,
-            'duplicatecourse',
-            'duplicatecourse',
-            new pix_icon('i/calendar', $label)
-        );
-        if ($PAGE->url->compare($url, URL_MATCH_BASE)) {
-            $newnode->make_active();
-        }
-        $settingnode->add_node($newnode);
+
+    $label = get_string('coursedeletionsettings', 'local_coursedeletion');
+    $url = new moodle_url('/local/coursedeletion/coursesettings.php', array('id' => $PAGE->course->id));
+    $newnode = navigation_node::create(
+        $label,
+        $url,
+        navigation_node::NODETYPE_LEAF,
+        'duplicatecourse',
+        'duplicatecourse',
+        new pix_icon('i/calendar', $label)
+    );
+    if ($PAGE->url->compare($url, URL_MATCH_BASE)) {
+        $newnode->make_active();
     }
+    $parentnode->add_node($newnode);
+    
 }
