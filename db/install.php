7c317ca6b83221b858b9b1b26002c1ecb73d921f
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2018-11-16 09:51:32 +0100

    Add missing copyright headers; remove pre-3.1 patch

--- db/install.php
+++ db/install.php
@@ -1,4 +1,25 @@
 <?php
+// This file is part of local/coursedeletion
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package local/coursedeletion
+ * @copyright 2014-2018 Liip AG <https://www.liip.ch/>
+ * @author Brian King <brian.king@liip.ch>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -12,10 +33,10 @@ function xmldb_local_coursedeletion_install() {
     set_config('interval_notification_before_enddate', 'P3W', 'local_coursedeletion');
     set_config('interval_staged_to_deletion', 'P3M', 'local_coursedeletion');
     set_config('school_contact_url', 'http://web.fhnw.ch/e-learning', 'local_coursedeletion');
-    local_courseduplication_add_course_deletion_records();
+    local_coursedeletion_add_course_deletion_records();
 }
 
-function local_courseduplication_add_course_deletion_records() {
+function local_coursedeletion_add_course_deletion_records() {
     global $DB;
     require_once(__DIR__ . '/../locallib.php');
 
--- version.php
+++ version.php
@@ -1,9 +1,29 @@
 <?php
+// This file is part of local/coursedeletion
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package local/coursedeletion
+ * @copyright 2014-2018 Liip AG <https://www.liip.ch/>
+ * @author Brian King <brian.king@liip.ch>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 defined('MOODLE_INTERNAL') || die();
 
-
-$plugin->version = 2015032700;//Required - the version number of your plugin in the form YYYYMMDDxx, with xx=01 indicating this is the first version for that day
-$plugin->requires = 2014051200;//Optional - minimum version number of Moodle that this plugin requires
-$plugin->component = 'local_coursedeletion'; // Full name of the plugin (used for diagnostics): plugintype_pluginname
-$plugin->maturity = MATURITY_BETA;//Optional - how stable the plugin is:
+$plugin->version   = 2018111600;
+$plugin->requires  = 2016052300; // Moodle 3.1+.
+$plugin->component = 'local_coursedeletion';
+$plugin->maturity  = MATURITY_BETA;
